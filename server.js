aaa42c2f29d64142d6226e4e7d78608196d593e9
opeh sandrine <agborsandrine31@gmail.com>
2026-05-17 23:22:12 +0100

    MongoDB connection  and env configuration done

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const app = require("./src/app");
+const connectDB = require("./src/config/db");
 
 const PORT = process.env.PORT || 3000;
+
+// Connect to database FIRST
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
